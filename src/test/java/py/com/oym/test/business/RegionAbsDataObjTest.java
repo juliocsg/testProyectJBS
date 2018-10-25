d1fcb03c25e0da645d4607fb2ed044d0a2d302b4
OYM-DEV7 <oymdev7@oym.com.py>
2018-10-25 08:56:44 -0300

    JBS Business test AbstractDataObject

--- src/test/java/py/com/oym/test/business/RegionAbsDataObjTest.java
+++ src/test/java/py/com/oym/test/business/RegionAbsDataObjTest.java
@@ -5,10 +5,7 @@
  */
 package py.com.oym.test.business;
 
-import java.math.BigDecimal;
 import javax.naming.NamingException;
-import org.javabeanstack.data.IDataResult;
-import org.javabeanstack.data.IDataRow;
 import org.javabeanstack.data.IDataSet;
 import org.javabeanstack.data.IGenericDAORemote;
 import org.javabeanstack.data.model.DataSet;
@@ -17,7 +14,6 @@ import org.javabeanstack.datactrl.IDataObject;
 import org.javabeanstack.exceptions.SessionError;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.javabeanstack.model.tables.Moneda;
 import org.javabeanstack.model.tables.Pais;
 import org.javabeanstack.model.tables.Region;
 import static py.com.oym.test.business.TestClass.dataLink;
@@ -33,7 +29,7 @@ public class RegionAbsDataObjTest extends TestClass {
     @Test
     public void test1AddDataRegion() throws NamingException, SessionError, Exception 
     {
-        System.out.println("Test Moneda Persist");
+        System.out.println("test1AddDataRegion");
         if (error != null) {
             System.out.println(error);
             return; 
@@ -46,14 +42,13 @@ public class RegionAbsDataObjTest extends TestClass {
         region.setField("nombre", "Pacífico");
         DataSet dataSet = new DataSet();
         dataSet.addDataObject("region", region);
-        
         boolean resultado = dataLink.update(dataSet).isSuccessFul();
         assertTrue(resultado);
     }
     @Test
-    public void test2DeleteRegion() throws NamingException, SessionError, Exception{
+    public void test2DeleteDataRegion() throws NamingException, SessionError, Exception{
         if (error != null){
-            System.out.println("test2DeleteRegion");
+            System.out.println("test2DeleteDataRegion");
             return;
         }
         boolean resultadoado;
@@ -68,22 +63,13 @@ public class RegionAbsDataObjTest extends TestClass {
         }
     }
     @Test
-    public void test3AddDataRegionMonPais() throws NamingException, SessionError, Exception {
-        System.out.println("testUpdatePaisDataObject");
+    public void test3AddDeleteDataRegionPais() throws NamingException, SessionError, Exception {
+        System.out.println("test3AddDeleteDataRegionPais");
         //No hubo conexión con el servidor de aplicaciones
         if (error != null) {
             System.out.println(error);
             return;
         }
-        boolean resultado;
-
-        //Moneda
-        IDataObject moneda = new DataObject(Moneda.class, null, dataLink, null);
-        moneda.open();
-        moneda.insertRow();
-        moneda.setField("codigo", "MXN");
-        moneda.setField("nombre", "Mexicanos");
-
         //Region
         IDataObject region = new DataObject(Region.class, null, dataLink, null);
         region.open();
@@ -98,61 +84,26 @@ public class RegionAbsDataObjTest extends TestClass {
         pais.setField("codigo", "124");
         pais.setField("nombre", "Perú");
         pais.setField("region", region.getRow());
-        pais.setField("region", moneda.getRow());
 
         IDataSet dataSet = new DataSet();
         dataSet.addDataObject("region", region);
         dataSet.addDataObject("pais", pais);
-
-        boolean result = dataLink.update(dataSet).isSuccessFul();
-        System.out.println(result);
-        assertTrue(result);
-
         
-        region.find("codigo", "PCF");
-        region.deleteRow();
-        resultado = region.update(false);
-        if (!resultado) {
-            System.out.println(region.getErrorMsg(true));
-        }
+        boolean resultado = dataLink.update(dataSet).isSuccessFul();
+        System.out.println(resultado);
+        assertTrue(resultado);
+        
         pais.find("codigo", "124");
         pais.deleteRow();
         resultado = pais.update(false);
         if (!resultado) {
             System.out.println(pais.getErrorMsg(true));
         }
-    }
-
-    @Test
-    public void test4DeleteRegionMoneda() throws NamingException, SessionError, Exception {
-        System.out.println("test3AddDataRegionMoneda");
-        if (error != null) {
-            System.out.println(error);
-            return;
+        region.find("codigo", "PCF");
+        region.deleteRow();
+        resultado = region.update(false);
+        if (!resultado) {
+            System.out.println(region.getErrorMsg(true));
         }
-        boolean resultado;
-        //Moneda
-
-        //Region
-        IDataObject region = new DataObject(Region.class, null, dataLink, null);
-        region.open();
-        if (region.find("codigo", "PCF")) {
-            region.deleteRow();
-            resultado = region.update(false);
-            if (!resultado) {
-                System.out.println(region.getErrorMsg(true));
-            }
-        }
-        //Pais
-        IDataObject pais = new DataObject(Pais.class, null, dataLink, null);
-        pais.open();
-        if (pais.find("codigo", "124")) {
-            pais.deleteRow();
-            resultado = pais.update(false);
-            if (!resultado) {
-                System.out.println(pais.getErrorMsg(true));
-            }
-        }
-
     }
 }
